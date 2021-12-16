5ea671fb75e2afdd3018a7ca0cd075d167689a4f
Daniel Idén <daniel@illumen.se>
2021-12-16 02:04:43 +0100

    v1.0.3 added extensions methods for json parsing

---
+++ OAuth2HttpClient/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace OAuth2HttpClientNS;
+
+public static class Extensions
+{
+    public static async IAsyncEnumerable<TResult?> GetJsonResponseAsAsyncEnumerable<TResult>(
+        this OAuth2HttpClient client, string requestUri, JsonSerializerOptions? jsonSerializerOptions = null,
+        [EnumeratorCancellation] CancellationToken ct = default)
+    {
+        var response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead, ct);
+        response.EnsureSuccessStatusCode();
+
+        await using var stream = await response.Content.ReadAsStreamAsync(ct);
+        await foreach (var item in JsonSerializer.DeserializeAsyncEnumerable<TResult>(stream,
+                           jsonSerializerOptions, ct))
+        {
+            yield return item;
+        }
+    }
+
+    public static async IAsyncEnumerable<TResult?> ParseJsonResponseAsAsyncEnumerable<TResult>(
+        this HttpResponseMessage response, JsonSerializerOptions? jsonSerializerOptions = null,
+        [EnumeratorCancellation] CancellationToken ct = default)
+    {
+        response.EnsureSuccessStatusCode();
+
+        await using var stream = await response.Content.ReadAsStreamAsync(ct);
+        await foreach (var item in JsonSerializer.DeserializeAsyncEnumerable<TResult>(stream,
+                           jsonSerializerOptions, ct))
+        {
+            yield return item;
+        }
+    }
+}
